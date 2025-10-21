89217af1a059d4f6cb17ec3ecf8fa70075354f04
nabil258766-source <nabil258766@student.smkn1kandeman.sch.id>
2025-10-21 09:30:50 +0700

    Add files via upload

---
+++ nabil/praktik 3.9/praktik 3.9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penugasan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== contoh operator penugasan ===");
+
+            int a = 10;
+            Console.WriteLine("nilai awal a = " + a);
+
+            // operator penugasan 
+            a += 5;  // a = a + 5
+            Console.WriteLine("setelah a += 5, nilai a = " + a);
+
+            a -= 3;  // a = a - 3
+            Console.WriteLine(setelah a -= 3, 
+
+        }
+    }
+}
---
+++ nabil/praktik_6.1_nabil_xpplg 2/praktik_6.1_nabil_xpplg 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._1_nabil_xpplg_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Perulangan untuk menampilkan angka 1 sampai 10: ");
+
+            // perulangan for dimulai dari disini
+            for (int i = 1; i <= 10; i++)
+            {
+                // Blok kode yang akan diulang 
+                Console.WriteLine("angka ke-" + i);
+            }
+
+            Console.WriteLine("\nPerulangan selesai.");
+        }
+    }
+}
